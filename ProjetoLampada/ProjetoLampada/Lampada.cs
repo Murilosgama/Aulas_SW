edf63a6fe495c0df644cba2cfb08ccef7b8948e1
Murilo Gama <124800631+Murilosgama@users.noreply.github.com>
2024-03-01 12:41:33 -0300

    Add files via upload

---
+++ ProjetoLampada/ProjetoLampada/Lampada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLampada
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private int Potencia { get; set; }
+        private string Cor { get; set; }
+        public void ligar()
+        {
+            Ligada = true;
+        }
+        public void desligar()
+        {
+            Ligada = false;
+        }
+        public bool estaLigada()
+        {
+            return Ligada;      
+        }
+
+    }
+}
---
+++ ProjetoLampada/ProjetoLampada/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjetoLampada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada lamp1 = new Lampada();
+
+            lamp1.ligar();
+            lamp1.desligar();
+            Console.WriteLine("A lampada está ligada?" + lamp1.estaLigada());
+            
+
+
+        }
+    }
+}
