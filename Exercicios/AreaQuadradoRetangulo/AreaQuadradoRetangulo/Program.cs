9b4ea6be8be52dd11634559f43710ab7de79b6f1
Murilo Gama <124800631+Murilosgama@users.noreply.github.com>
2024-04-05 12:44:58 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de area, Quadradro ou Retangulo.");
+            Area area= new Area();
+            Console.WriteLine("Digite o valor da base:");
+            area.Base = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor da altura:");
+            area.Altura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O valor da area: " + area.Calcular2());
+
+            area.MostraDetalhes();
+
+        }
+    }
+}
---
+++ Exercicios/Exencicio2/Exencicio2/Area.cs
@@ -0,0 +1,41 @@
+﻿namespace Exencicio2
+{
+    public class Area
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+        public float AreaCalculo { get; set; }
+
+        public void Escolha(int opcao)
+        {
+            if (opcao == 1)
+            {
+                float quadrado CalculoAreaQuadrado();
+                Console.WriteLine("Area do quadrado é " + quadrado);
+            }
+            if (opcao == 2)
+            {
+                float retangulo CalculoAreaRetangulo();
+                Console.WriteLine("Area do retangulo é " + retangulo);
+            }
+            if (opcao == 3)
+            {
+                float triangulo CalculoAreaTriangulo();
+                Console.WriteLine("Area do triangulo é " + triangulo);
+            }
+        }
+
+        public float CalculoAreaQuadrado()
+        {
+            return AreaCalculo;
+        }
+        public float CalculoAreaRetangulo()
+        {
+            return AreaCalculo;
+        }
+        public float CalculoAreaTriangulo()
+        {
+            return AreaCalculo;
+        }
+    }
+}
