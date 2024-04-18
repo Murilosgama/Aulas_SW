8277820f801ea4193cae532616b6bc5c52e44193
Murilo Gama <124800631+Murilosgama@users.noreply.github.com>
2024-04-18 07:56:46 -0300

    Add files via upload

---
+++ Projetos/contaBancaria/contaBancaria/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace contaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao Banco da Lelê do Grau!");
+
+            //instanciar a classe Conta e criar um objeto dela
+            Conta conta = new Conta();
+            Console.WriteLine("Consultando o saldo");
+            conta.consulta_saldo();
+
+            Console.WriteLine("Depositando de R$1000.55");
+            conta.depositar(1000.35);
+            Console.WriteLine("Consultando o saldo");
+            conta.consulta_saldo();
+
+            //Console.WriteLine("Sacando R$500.55");
+            //conta.sacar(500);
+            //Console.WriteLine("Consultando o saldo");
+            //conta.consulta_saldo();
+
+            //Console.WriteLine("Sacando 1000");
+            //conta.sacar(1000);
+
+            Console.WriteLine("Definindo limite de 1000");
+            conta.ajusta_limite(1000);
+            conta.consulta_saldo();
+            conta.sacar(2000);
+            conta.consulta_saldo();
+        }
+    }
+}
---
+++ Site/Site/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Site
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Informacoes p = new Informacoes();
+            Console.WriteLine(p.FL());
+        }
+    }
+}
